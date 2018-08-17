9ba2dd45a2758d2837cc1861f80275342f4b7d5c
xiaoyingza <atlas1873879592@gmail.com>
2018-08-17 15:54:16 +0800

    fixed unit_test

--- unit_test.py
+++ unit_test.py
@@ -7,16 +7,20 @@
 
 
 import unittest
+
+
 class Student(object):
     def __init__(self, name, score):
         self.name = name
         self.score = score
 
     def get_grade(self):
-        if self.score >= 60:
-            return 'B'
+        if self.score < 0 or self.score > 100:
+            raise ValueError
         if self.score >= 80:
             return 'A'
+        if self.score >= 60:
+            return 'B'
         return 'C'
 
 
