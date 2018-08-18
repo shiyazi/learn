3193d98d13d30a48e51e42bfd4819ec3decfc5c6
xiaoyingza <atlas1873879592@gmail.com>
2018-08-18 14:13:04 +0800

    finish the find_file

--- IO_Programming/find_file.py
+++ IO_Programming/find_file.py
@@ -7,6 +7,20 @@
 
 
 import os
+
+aim = 'readme'
 path1 = '/Persimmon'
-L = [x for x in os.listdir(path1) if os.path.isdir(os.path.join(path1, x))]
+L = [x for x in os.listdir(path1) if os.path.isdir(os.path.join(path1, x))]  # 输出目录下子目录名称
 print(L)
+
+for root, dirs, files in os.walk(top=path1, topdown=True):
+    for file in files:
+        if aim in file:
+            print(root)
+            with open(os.path.join(root,'readme.txt'),'r') as f:
+                print(f.read())
+    continue
+
+print(os.path.abspath('.'))
+path2 = os.path.abspath('..')  # 当前目录的上一目录路径
+
